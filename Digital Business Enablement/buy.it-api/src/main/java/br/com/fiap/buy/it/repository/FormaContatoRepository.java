a3ed8332521ce6d5ceef5c06dadba0d84a1515ef
KaueCaponero <kauecaponero@hotmail.com>
2023-11-27 15:29:47 -0300

    Modificando Repositorys

--- Digital Business Enablement/buy.it-api/src/main/java/br/com/fiap/buy/it/repository/FormaContatoRepository.java
+++ Digital Business Enablement/buy.it-api/src/main/java/br/com/fiap/buy/it/repository/FormaContatoRepository.java
@@ -1,18 +1,10 @@
 package br.com.fiap.buy.it.repository;
 
 import br.com.fiap.buy.it.model.FormaContato;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 
 @Repository
 public interface FormaContatoRepository extends JpaRepository<FormaContato, Long> {
-    Optional<FormaContato> findById(Long id);
 
-    @Query("SELECT c FROM FormaContato c WHERE c.id = ?1")
-    Page<FormaContato> findByUserIdWithPagination(Long id, Pageable pageable);
 }
